b8e30453e3fa38d8c35cc39c90114b753982c955
Jason El-Massih <jel-massih@hotmail.com>
2013-09-10 21:38:39 -0400

    Fixing GithubUpload (added space in Sports Wall)

--- SportsWall_Site/githubupload.php
+++ SportsWall_Site/githubupload.php
@@ -4,37 +4,38 @@ $rawurl = "";
 
 if (isset($_POST['payload'])) {
 	$payload = json_decode(stripslashes($_POST['payload']));
-	
+
 	$url = $payload->repository->url;
+	global $rawurl;
 	$rawurl = str_replace("https://", "https://raw.", $url);
-	$rawurl = $rawurl."/";
+	$rawurl = $rawurl."/master/SportsWall_Site/";
+	
 	foreach($payload->commits as $commit) {
-		debug($commit->message);
 		$added = $commit->added;
 		$removed = $commit->removed;
 		$changed = $commit->modified;
 		
 		foreach($added as $add) {
-			debug("ADD: ".$add);
 			addFile($add);
 		}
 		foreach($changed as $change) {
-			debug("Change: ".$change);
 			addFile($change);
 		}
 		foreach($removed as $remove) {
-			debug("REM: ".$add);
 			removeFile($remove);
 		}
 	}
 }
 
 function addFile($path) {
-	if(stripos($path, "Node_Server/")) {return;}
+	global $rawurl;
+	
+	if(stripos($path, "Node_Server/") != false){return;}
 	$path = str_replace("SportsWall_Site/", "", $path);
 	if(!file_exists(dirname($path))) {
     	mkdir(dirname($path), 0777, true);
 	}
+	debug("Saving: ".$rawurl.$path." to ".$path);
 	file_put_contents($path, fopen($rawurl.$path, 'r'));
 }
 
@@ -44,7 +45,6 @@ function removeFile($path) {
 	}
 }
 
- 
 function debug($txt) {
 	file_put_contents("log.txt",$txt."\n", FILE_APPEND | LOCK_EX);
 }
